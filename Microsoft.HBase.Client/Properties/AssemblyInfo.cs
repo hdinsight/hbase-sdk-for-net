640447809bbc31a542df5598ce20ba58502f62c9
mwprochaska <mwprochaska@outlook.com>
2014-07-31 14:17:23 -0700

    added nuspec for maxluk review changed version number to 0.1 fixed test ruleset reference for release builds

--- Microsoft.HBase.Client/Properties/AssemblyInfo.cs
+++ Microsoft.HBase.Client/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: InternalsVisibleTo("Microsoft.HBase.Client.Tests")]
